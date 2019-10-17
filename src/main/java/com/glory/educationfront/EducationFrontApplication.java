3f5b4142d90597e86848168a0da72aa2cd8af90a
Administrator <1528850355@qq.com>
2019-10-17 22:33:37 +0800

    教育前端项目

---
+++ src/main/java/com/glory/educationfront/EducationFrontApplication.java
@@ -0,0 +1,13 @@
+package com.glory.educationfront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducationFrontApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EducationFrontApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/glory/educationfront/EducationFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glory.educationfront;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducationFrontApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
